8e8b4ebf395086e126fe79b81152c0f5811f8265
matiasal55 <matiasal55@users.noreply.github.com>
2019-10-16 14:42:21 -0300

    Clase Persona

--- src/pb2/puntos/Persona.java
+++ src/pb2/puntos/Persona.java
@@ -1,5 +1,24 @@
 package pb2.puntos;
 
-public class Persona {
-
+public class Persona extends Usuario{
+	private String nombre;
+	private String apellido;
+	public Persona(String mail, String contrasenia, String nombre, String apellido) {
+		super(mail, contrasenia);
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	
 }
